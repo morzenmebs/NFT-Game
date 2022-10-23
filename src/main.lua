1696305ce5f248b9527824d50201c48fb02f6acd
morzenmebs <33324453+morzenmebs@users.noreply.github.com>
2022-10-23 14:21:43 -0500

    add tiles

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,10 @@
-function love.load()
+require("tilesheet")
+--require("player")
 
+function love.load()
+    grass = love.graphics.newImage("sprites/TestGrass64.png")
+    path = love.graphics.newImage("sprites/TestPath64.png")
+    tilesize = 64
 end
 
 function love.update(dt)
@@ -7,6 +12,10 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.rectangle("line", 100, 50, 200, 150)
+    for i,row in ipairs(tilesheet) do
+        for j,tile in ipairs(row) do
+           love.graphics.draw(tile, j*tilesize, i*tilesize)
+        end
+    end 
 end
 
---
+++ src/tilesheet.lua
@@ -0,0 +1,9 @@
+tilesheet = {
+    {grass, grass, grass, road, grass, grass, grass}, 
+    {grass, grass, grass, road, grass, grass, grass}, 
+    {grass, grass, grass, road, grass, grass, grass}, 
+    {road, road, road, road, road, road, road},
+    {grass, grass, grass, road, grass, grass, grass}, 
+    {grass, grass, grass, road, grass, grass, grass}, 
+    {grass, grass, grass, road, grass, grass, grass} 
+}
